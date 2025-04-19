561978a2e7395f6a40882dc1dfb76fa5615ed0f3
Zainab-Shakeel <zainabshakeel400@gmail.com>
2025-04-20 02:00:04 +0500

    new functionality "add new Product" added in Products.jsx

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -34,6 +34,15 @@ function Products() {
 
     const [searchTerm, setSearchTerm] = useState('');
 
+    const [showAddModal, setShowAddModal] = useState(false);
+
+    const [newProduct, setNewProduct] = useState({
+        name: '',
+        category: '',
+        price: '',
+        stock: '',
+    })
+
     const handleEditClick = (product) => {
         seteditingProduct(product)
         setShowEditModal(true);
@@ -52,14 +61,21 @@ function Products() {
     return (
         <div>
             <h1 className="text-3xl font-bold mb-6">Products</h1>
-            <div className="mb-4">
+            <div className="flex items-center justify-between mb-4">
                 <input
                     type="text"
-                    placeholder="Search by name..."
-                    className="w-full max-w-xs border px-4 py-2 rounded"
+                    placeholder="Search product..."
+                    className="border px-3 py-2 rounded w-1/3"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
+
+                <button
+                    onClick={() => setShowAddModal(true)}
+                    className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+                >
+                    + Add Product
+                </button>
             </div>
             <div className="overflow-x-auto bg-white rounded-lg shadow-md text-black">
                 <table className="min-w-full text-left text-sm">
@@ -127,6 +143,10 @@ function Products() {
                                 <label className="block mb-1 font-medium">Price </label>
                                 <input className="w-full border px-3 py-2 rounded" type="number" value={editingProduct.price} onChange={(e) => seteditingProduct({ ...editingProduct, price: parseFloat(e.target.value) })} />
                             </div>
+                            <div>
+                                <label className="block mb-1 font-medium">Quantity </label>
+                                <input className="w-full border px-3 py-2 rounded" type="number" value={editingProduct.stock} onChange={(e) => seteditingProduct({ ...editingProduct, stock: parseInt(e.target.value) })} />
+                            </div>
                             <div className="text-white flex justify-end gap-3 p-2">
                                 <button className="px-4 py-2 rounded" type="buton" onClick={() => setShowEditModal(false)}>
                                     Cancel
@@ -164,6 +184,99 @@ function Products() {
                         </div>
                     </div>
                 </div>
+            )}
+
+            {/* Add product modal */}
+            {showAddModal && (
+                <div className="text-black fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                    <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+                        <h2 className="text-xl font-bold mb-4">Add new Product</h2>
+
+                        <form
+                            onSubmit={(e)=>{
+                                e.preventDefault();
+                                const newId= products.length ? products[products.length - 1].id + 1 : 1;
+                                const productToAdd={
+                                    ...newProduct,
+                                    id: newId,
+                                    price: parseFloat(newProduct.price),
+                                    stock: parseInt(newProduct.stock),
+                                }
+
+                                setProducts([...products, productToAdd]);
+                                setShowAddModal(false);
+                                setNewProduct({name: '', category: '', price: '', stock: ''});
+                            }}
+                            className="space-y-4"
+                        >
+                            <div>
+                                <label><strong>Product Name </strong></label>
+                                <input 
+                                    type="text" 
+                                    className="w-full border px-3 py-2 rounded" 
+                                    value={newProduct.name}
+                                    onChange={
+                                        (e)=>{
+                                            setNewProduct({...newProduct, name: e.target.value})
+                                        }
+                                    }
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label><strong>Category </strong></label>
+                                <input 
+                                    type="text" 
+                                    className="w-full border px-3 py-2 rounded" 
+                                    value={newProduct.category}
+                                    onChange={
+                                        (e)=>{
+                                            setNewProduct({...newProduct, category: e.target.value})
+                                        }
+                                    }
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label><strong>Price </strong></label>
+                                <input 
+                                    type="number" 
+                                    className="w-full border px-3 py-2 rounded" 
+                                    value={newProduct.price}
+                                    onChange={
+                                        (e)=>{
+                                            setNewProduct({...newProduct, price: e.target.value})
+                                        }
+                                    }
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label><strong>Quantity </strong></label>
+                                <input 
+                                    type="number" 
+                                    className="w-full border px-3 py-2 rounded" 
+                                    value={newProduct.stock}
+                                    onChange={
+                                        (e)=>{
+                                            setNewProduct({...newProduct, stock: e.target.value})
+                                        }
+                                    }
+                                    required
+                                />
+                            </div>
+                            <div className="text-white flex justify-end gap-3 p-2">
+                                <button className="text-white px-4 py-2" type="button" onClick={() => setShowAddModal(false)}>
+                                    Cancel
+                                </button>
+                                <button className="text-white px-4 py-2" type="submit">
+                                    Add Product
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
             )}
         </div>
     )
